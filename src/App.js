46402cf97c5ed18544572eb48c4317dbf768467a
Atila <atilafb77@gmail.com>
2022-03-08 09:18:36 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Deck from './components/Deck';
+
+function App() {
+  return (
+    <div>
+      <Deck />
+    </div>
+  );
+}
+
+export default App;
