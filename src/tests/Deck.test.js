5cd1bb7f0b0721532524a408bf69bca355b33fec
Atila <atilafb77@gmail.com>
2022-03-09 19:14:26 -0300

    fix: adjust test files

--- src/tests/Deck.test.js
+++ src/tests/Deck.test.js
@@ -1,22 +1,27 @@
-import { Deck, createDeck, setupShuffledDeckMap, setupCutDeckMap } from '../components/Deck'
+import { dealCards, shuffle } from '../utils/DeckManager'
 
-describe("setupShuffledDeckMap", () => {
+describe("shuffle", () => {
     it("shuffles the entire deck and check if all cards are still there", () => {
-        let deckMap = createDeck();
-        let shuffleDeck = setupShuffledDeckMap(deckMap);
-        expect(shuffleDeck).toHaveLength(52);
+        const deck = [1, 2, 3, 4, 5]
+        const shuffleDeck = shuffle(deck);
+        expect(shuffleDeck).toHaveLength(5);
     });
     it("shuffled deck is not equal original deck", () => {
-        let deckMap = createDeck();
-        let shuffleDeck = setupShuffledDeckMap(deckMap);
-        expect(shuffleDeck).not.toEqual(deckMap);
+        let deck = [1, 2, 3, 4, 5]
+        let shuffleDeck = shuffle(deck);
+        expect(shuffleDeck).not.toEqual(deck);
+    });
+    it("shuffled deck contains the same cards", () => {
+        let deck = [1, 2, 3, 4, 5];
+        let shuffleDeck = shuffle(deck);
+        expect(shuffleDeck).toEqual(expect.arrayContaining(deck));
     });
 });
 
 describe("setupCutDeckMap", () => {
-    it("shuffles the deck and cut to have only 21 cards", () => {
-        let deckMap = createDeck();
-        let cutDeck = setupCutDeckMap(deckMap);
-        expect(cutDeck).toHaveLength(21);
+    it("shuffles the deck and cut to have only the upper cards", () => {
+        let deck = [1, 2, 3, 4, 5];
+        let cutDeck = dealCards(deck, 3);
+        expect(cutDeck).toEqual([1, 2, 3]);
     });
 });
