735f051f8557bfc3d5f04c5fe0856b2b5acc1352
Atila <atilafb77@gmail.com>
2022-03-11 13:50:33 -0300

    add button to each row

--- src/components/DeckRows.js
+++ src/components/DeckRows.js
@@ -1,8 +1,13 @@
 import DeckCard from "./DeckCard"
 
 const DeckRows = ({ cards }) => {
+  const handleClick = () => {
+    console.log("fui clicado")
+  }
+
   return (
     <div className="deck-row">
+      <button onClick={() => handleClick()}>Row of my card</button>
       {
         cards.map((item) => {
           return <DeckCard key={item.suit + item.value} {...item} />
